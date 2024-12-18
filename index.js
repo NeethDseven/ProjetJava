b015d8a6823490ce12879cf2ec03a0d27468afb3
Neeth Dseven <labidi.neeth@gmail.com>
2024-12-18 13:52:49 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const totalQuestions = 10;
-  let currentQuestion = 1;
-  const correctAnswers = ['Pacifique',
+document.addEventListener('DOMContentLoaded', () => {
+  const totalQuestions = 10
+  let currentQuestion = 1
+  const correctAnswers = [
+    'Pacifique',
     'Seconde',
     'Vinci',
     'Amazone',
@@ -10,59 +11,60 @@ document.addEventListener('DOMContentLoaded', function () {
     'Fleming',
     'Mandarin',
     'Azote',
-    'Lune'];
+    'Lune'
+    ]
 
-  let userAnswers = Array.from({ length: totalQuestions }, () => null);
-  const progressBar = document.querySelector('.progress-bar');
-  const nextButton = document.querySelector('#next-btn');
-  const prevButton = document.querySelector('#previous-btn');
+  let userAnswers = Array.from({ length: totalQuestions }, () => null)
+  const progressBar = document.querySelector('.progress-bar')
+  const nextButton = document.querySelector('#next-btn')
+  const prevButton = document.querySelector('#previous-btn')
 
   function updateProgressBar() {
-    const progress = ((currentQuestion - 1) / totalQuestions) * 100;
-    const roundedProgress = Math.ceil(progress / 10) * 10;
-    progressBar.style.width = `${roundedProgress}%`;
-    progressBar.textContent = `${roundedProgress}%`;
+    const progress = ((currentQuestion - 1) / totalQuestions) * 100
+    const barProgress = Math.ceil(progress / 10) * 10
+    progressBar.style.width = `${barProgress}%`
+    progressBar.textContent = `${barProgress}%`
   }
 
   function showTab(index) {
-    const tabs = document.querySelectorAll('.nav-link');
-    const tabContents = document.querySelectorAll('.tab-pane');
+    const tabs = document.querySelectorAll('.nav-link')
+    const tabContents = document.querySelectorAll('.tab-pane')
 
     tabs.forEach((tab, i) => {
-      tab.classList.toggle('active', i === index - 1);
-      tab.disabled = i !== index - 1;
+      tab.classList.toggle('active', i === index - 1)
+      tab.disabled = i !== index - 1
     });
 
     tabContents.forEach((content, i) => {
       if (i === index - 1) {
-        content.classList.add('show', 'active');
+        content.classList.add('show', 'active')
       } else {
-        content.classList.remove('show', 'active');
+        content.classList.remove('show', 'active')
       }
-    });
+    })
 
-    updateProgressBar();
+    updateProgressBar()
 
     if (index > totalQuestions) {
-      displayResults();
+      displayResults()
     }
   }
 
   function validateCurrentQuestion() {
-    const form = document.querySelector(`#form-question-${currentQuestion}`);
-    const checkedInput = form.querySelector('input[type="radio"]:checked');
+    const form = document.querySelector(`#form-question-${currentQuestion}`)
+    const checkedInput = form.querySelector('input[type="radio"]:checked')
     if (checkedInput) {
-      userAnswers[currentQuestion - 1] = checkedInput.value;
-      return true;
+      userAnswers[currentQuestion - 1] = checkedInput.value
+      return true
     }
-    return false;
+    return false
   }
 
   function displayResults() {
     const correctCount = userAnswers.filter((answer, index) => answer === correctAnswers[index]).length;
-    const incorrectCount = totalQuestions - correctCount;
+    const incorrectCount = totalQuestions - correctCount
 
-    const ctx = document.querySelector('#my-chart').getContext('2d');
+    const ctx = document.querySelector('#my-chart').getContext('2d')
     new Chart(ctx, {
       type: 'doughnut',
       data: {
@@ -84,26 +86,26 @@ document.addEventListener('DOMContentLoaded', function () {
           }
         }
       }
-    });
+    })
   }
 
-  nextButton.addEventListener('click', function () {
+  nextButton.addEventListener('click', () => {
     if (validateCurrentQuestion()) {
       if (currentQuestion < totalQuestions + 1) {
-        currentQuestion++;
-        showTab(currentQuestion);
+        currentQuestion++
+        showTab(currentQuestion)
       }
     } else {
-      alert('Réponse obligatoire pour continuer le questionnaire');
+      alert('Réponse obligatoire pour continuer le questionnaire')
     }
   });
 
-  prevButton.addEventListener('click', function () {
+  prevButton.addEventListener('click',() => {
     if (currentQuestion > 1) {
-      currentQuestion--;
-      showTab(currentQuestion);
+      currentQuestion--
+      showTab(currentQuestion)
     }
   });
 
-  showTab(currentQuestion);
-});
+  showTab(currentQuestion)
+})
